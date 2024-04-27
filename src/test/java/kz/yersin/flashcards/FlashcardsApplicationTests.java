b7b65b329c6f9fc4ceb01246393352e8e2fdc355
Yersin Mukai <mukaiyersin@gmail.com>
2024-04-27 21:15:05 +0300

    init project

---
+++ src/test/java/kz/yersin/flashcards/FlashcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.yersin.flashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
